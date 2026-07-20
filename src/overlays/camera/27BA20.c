d6298c4978cc660f9d7255ed8ee0c30490876c87
dreamsailing59-ops <dreamsailing59@gmail.com>
2026-07-20 20:13:01 +0700

    Matched func_cam_80288BB0 and func_cam_80289480

---
+++ src/overlays/camera/27BA20.c
@@ -0,0 +1,418 @@
+#include "common.h"
+
+#include "PR/sched.h"
+
+s32 func_cam_80285830(s32*, s32, s32, s32);
+extern s32 D_802C8760;
+extern s32 func_cam_802889A0;
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80284130.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802841CC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028429C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80284510.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80284620.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802846A4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80284AF0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80284EC8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80284FF0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285118.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285210.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285228.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028537C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802853B0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285558.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285590.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285684.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285708.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802857A0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285830.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802858AC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802858BC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802858F0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285900.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028593C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028596C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285980.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285B4C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285BB8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80285F1C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286060.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286178.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286880.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286A68.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286A78.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286B28.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286B70.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286C90.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286D44.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286D54.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286E20.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286E6C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286EAC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80286ED0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802870C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287170.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287290.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802872EC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287360.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802873F0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287490.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287704.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028772C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028773C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287760.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802877B8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802877E8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802878C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802879D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287D0C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287EA4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287EE0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287EF0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287F28.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80287F38.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802884E8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802885E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028863C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802886AC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80288944.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028897C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802889A0.s")
+
+s32 func_cam_80288BB0(s32 arg0) {
+    if (arg0 == 1) {
+        D_802C8760 = 0x10;
+        func_cam_80285830(&func_cam_802889A0, 0x3E7, 0, 0);
+        return -1;
+    }
+    return 0;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80288C18.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80288CC0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80288ED8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80288F5C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80289208.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80289464.s")
+
+void func_cam_80289480(s32 arg0, s32 arg1, s32 arg2, s32 arg3, struct { s32 unk0; s32 unk4; s32 unk8; } *arg4, s32 *arg5) {
+    u16 sp6 = ((u16 *)arg0)[(arg2 << 7) + arg1];
+    arg4->unk0 += (s32)((sp6 & 0x3E) * arg3);
+    arg4->unk4 += (s32)((sp6 & 0x7C0) * arg3);
+    arg4->unk8 += (s32)((sp6 & 0xF800) * arg3);
+    *arg5 += arg3;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028951C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802895D0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802898A4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80289A3C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028A24C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028A490.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028A58C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028A914.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028AFC0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028B2D0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028B3A8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028B488.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028C528.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028C950.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028CB24.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028CBE4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028CCA8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028CDBC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028CE1C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028CE88.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028CEFC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028CF9C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028D084.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028D0C8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028D10C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028D304.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028D6C4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028D77C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028D7D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028D88C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028DD18.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028DD84.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028DE00.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028DE88.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028E268.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028E38C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028E8A0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028EC10.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028EECC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028F1D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028FDC0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8028FF70.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80290220.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802902C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80290DC0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80290F28.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802911E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80291BB0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80292004.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80292200.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80292FAC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029302C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80293084.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802930DC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80293134.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80293340.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802935F0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802938A8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80293B60.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80294450.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80294DF8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029573C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802958AC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80295A1C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80295B80.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80295D30.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802960E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80296390.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802963D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802964D0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802966E4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029690C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80297260.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802978E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80297AB8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80297D40.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80298FE0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_802990D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80299354.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029965C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_80299960.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029A3D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029A418.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029A71C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029B920.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029BB94.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029BD44.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029BF9C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C20C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C258.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C2C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C328.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C3A4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C3F4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C488.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C4A8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C4E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C520.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C530.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029C804.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029CBE8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029CE98.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D378.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D618.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D688.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D730.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D780.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D818.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D838.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D868.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029D914.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029DB68.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029DB9C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029DBC0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029DC78.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029DD94.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029DEEC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029E080.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/camera/27BA20/func_cam_8029E140.s")
