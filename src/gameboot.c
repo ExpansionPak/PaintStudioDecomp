1c9b6c3fa1c2d21230275484b160b7870c778641
dreamsailing59-ops <dreamsailing59@gmail.com>
2026-07-22 18:51:23 +0700

    func_80004690 matched by @Bl00D4NGEL

--- src/gameboot.c
+++ src/gameboot.c
@@ -1213,7 +1213,10 @@ void func_80004594(char *text, s32 x, s32 y, u8 red, u8 green, u8 blue) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/gameboot/func_80004690.s")
+int func_80004690(s32 arg0) {
+    #define BETWEEN(x, min, max) ((x) >= (min) && (x) < (max))
+    return BETWEEN(arg0, 0x30, 0x3A) || BETWEEN(arg0, 0x61, 0x67) || BETWEEN(arg0, 0x41, 0x47);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/gameboot/func_800046DC.s")
 
