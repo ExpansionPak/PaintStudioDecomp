e2b1df35278a0715f99892ed611980ab2c3d5341
dreamsailing59-ops <dreamsailing59@gmail.com>
2026-07-22 12:02:26 +0700

    Matched func_saveload_802C1558

--- src/overlays/saveload/saveload.c
+++ src/overlays/saveload/saveload.c
@@ -877,7 +877,15 @@ void func_saveload_802C14A4(s32* arg0, s32 arg1) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlays/saveload/saveload/func_saveload_802C14F0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/saveload/saveload/func_saveload_802C1558.s")
+void func_saveload_802C1558(s32* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6) {
+    s32 sp1C;
+
+    sp1C = *arg0;
+    func_saveload_802C178C(0x64, 0x64, arg1, arg2);
+    func_saveload_802C1918(arg4, arg5, arg6, 0xFF);
+    func_saveload_802C1210(&sp1C, arg3);
+    *arg0 = sp1C;
+}
 
 void func_saveload_802C15D4(s32* arg0, s32 arg1, s32 arg2, s32 arg3) {
     s32 sp1C;
