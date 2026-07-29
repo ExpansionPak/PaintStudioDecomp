a01ae1be0e8687c37c6dea3056fc30e2711ab936
dreamsailing59-ops <dreamsailing59@gmail.com>
2026-07-29 08:24:11 +0700

    Matched __scTaskReady in libultra/sched/sched

---
+++ src/libultra/sched/sched.c
@@ -0,0 +1,51 @@
+#include "common.h"
+
+#include "PR/sched.h"
+
+OSScTask *__scTaskReady(OSScTask *t)
+{
+    int rv = 0;
+    void *a;
+    void *b;
+
+    if (t) {
+        if ((a = osViGetCurrentFramebuffer()) !=
+            (b = osViGetNextFramebuffer())) {
+            return 0;
+        }
+
+        return t;
+    }
+
+    return 0;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/__scExec.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/__scSchedule.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/func_8001B014.s")
+
+void sched_dummy_func0(void) {
+}
+
+void sched_dummy_func1(void) {
+}
+
+void sched_dummy_func2(void) {
+}
+
+void sched_dummy_func3(void) {
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/sched_unidentified_func0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/__scMain.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/osScGetCmdQ.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/osScRemoveClient.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/osScAddClient.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/sched/sched/osCreateScheduler.s")
